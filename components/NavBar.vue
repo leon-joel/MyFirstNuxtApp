cb6f49b0f0388ec9eec35bfd1c4eb9bf5b43f0c7
Leon <leopard10000@gmail.com>
2022-03-21 16:55:51 +0900

    layouts/default.vue を追加することでデフォルトViewをカスタマイズ

---
+++ components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav>
+      <!-- nuxt-linkタグをrouter-linkタグに変更しても動作します。
+      nuxt-linkでは後ほど出てくるprefetchの機能があります。
+      ページを開かなくても先にそのページのJavaScriptファイルをFetch(取得)しておく機能です。
+      またaタグに変更しても動作はしますが、
+      リンクをクリックする度にブラウザのリロードが行われページを構成するすべての情報をリロード毎に取得します。
+      通常のHTMLページ間での移動と同じ動作になります。 -->
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/about">About</nuxt-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
