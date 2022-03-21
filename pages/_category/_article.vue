b6ee0833c942c37ac5a7a70c1617fec729fbb9d8
Leon <leopard10000@gmail.com>
2022-03-22 00:59:43 +0900

    複数のパラメータを受け取り

---
+++ pages/_category/_article.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ this.$route.params.category }}/{{ this.$route.params.article }}</h1>
+</template>
+
+<script>
+export default {
+  // サーバー側とクライアント側で実行
+  created() {
+    console.log('created');
+    console.log(this.$route);
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/_category/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>カテゴリー:{{ this.$route.params.category }}</h1>
+</template>
+
+<script>
+export default {
+  // サーバー側とクライアント側で実行
+  created() {
+    console.log('created');
+    console.log(this.$route);
+  },
+}
+</script>
+
+<style>
+
+</style>
