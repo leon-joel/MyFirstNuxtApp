68c12a6eeb9f716dc2cd4636bf8e4ea463a8e680
Leon <leopard10000@gmail.com>
2022-03-22 00:23:46 +0900

    Dynamic Routes(動的ルート)
    this.$routeの中身

---
+++ pages/users/_id.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>ユーザID: {{ this.$route.params.id }}, name: {{ user.name }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      users: [
+        {
+          id: 1, name: 'John Doe'
+        },
+        {
+          id: 2, name: 'Kevin Smith',
+        },
+        {
+          id: 3, name: 'Harry Bosch'
+        },
+      ],
+    }
+  },
+  computed: {
+    user() {
+      return this.users.find(user => user.id == this.id);
+    }
+  },
+
+  mounted() {
+    console.log(this.$route);
+  }
+}
+</script>
+
+<style>
+</style>
